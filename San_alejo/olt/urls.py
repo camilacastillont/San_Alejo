b5dc7b7a59c0bfed9c86e2d87ab8e6bccb84bba6
camilacastillont <camilacastillont@gmail.com>
2025-10-16 10:41:35 -0600

    Agregando nuevas urls
    -Detalle tarjeta
    -Eliminar tarjeta
    -Agregar puerto
    -Agregar ont

--- San_alejo/olt/urls.py
+++ San_alejo/olt/urls.py
@@ -4,9 +4,11 @@ from . import views
 app_name = 'olt'
 
 urlpatterns = [
-   # path('', views.index, name='olt'),
+   
     path('', views.index, name='san_alejo'),
     path('agregar/', views.agregar_tarjeta, name='agregar_tarjeta'),
-    path("tarjeta/<int:pk>/", views.detalle_tarjeta, name="detalle_tarjeta"),
+    path('tarjeta/<int:tarjeta_id>/', views.detalle_tarjeta, name='detalle_tarjeta'),
     path('eliminar_tarjeta/<int:pk>/', views.eliminar_tarjeta, name='eliminar_tarjeta'),
+    path('tarjeta/<int:tarjeta_id>/puerto/agregar/', views.agregar_puerto, name='agregar_puerto'),
+    path('tarjetas/puerto/<int:puerto_id>/ont/agregar/', views.agregar_ont, name='agregar_ont'),
 ]

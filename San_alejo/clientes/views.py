d52c92c900ea8c226d72872a35e388b19fc319ed
camilacastillont <camilacastillont@gmail.com>
2025-10-16 13:48:42 -0600

    Update views.py

--- San_alejo/clientes/views.py
+++ San_alejo/clientes/views.py
@@ -50,3 +50,38 @@ def agregar_cliente(request):
         }
         return JsonResponse(data)
     return JsonResponse({"error": "Método no permitido"}, status=400)
+
+def detalle_cliente(request, cliente_id):
+    cliente = get_object_or_404(Cliente, id=cliente_id)
+    return render(request, 'clientes/detalle_clientes.html', {'cliente': cliente})
+
+def actualizar_cliente(request, cliente_id):
+    if request.method == 'POST':
+        cliente = get_object_or_404(Cliente, id=cliente_id)
+        data = request.POST
+
+        # Actualizar campos
+        cliente.nombre = data.get('nombre')
+        cliente.genero = data.get('genero')
+        cliente.fecha_nacimiento = data.get('fecha_nacimiento')
+        cliente.telefono = data.get('telefono')
+        cliente.celular = data.get('celular')
+        cliente.correo = data.get('correo')
+        cliente.lugar_trabajo = data.get('lugar_trabajo')
+        cliente.telefono_trabajo = data.get('telefono_trabajo')
+        cliente.tipo_cliente = data.get('tipo_cliente')
+        cliente.estado_civil = data.get('estado_civil')
+        cliente.cargo_trabajo = data.get('cargo_trabajo')
+        cliente.direccion = data.get('direccion')
+        cliente.colonia = data.get('colonia')
+        cliente.municipio = data.get('municipio')
+        cliente.ancho_bajada = data.get('ancho_bajada')
+        cliente.ancho_subida = data.get('ancho_subida')
+        cliente.paquete = data.get('paquete')
+        cliente.olt = data.get('olt')
+        cliente.referencia_nombre = data.get('referencia_nombre')
+        cliente.referencia_telefono = data.get('referencia_telefono')
+        cliente.save()
+
+        return JsonResponse({'success': True, 'message': 'Cliente actualizado correctamente'})
+    return JsonResponse({'success': False, 'message': 'Método no permitido'})

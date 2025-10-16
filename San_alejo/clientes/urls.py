56ee0cff8d9d7ab37eff2315a7bad08dee3aee77
camilacastillont <camilacastillont@gmail.com>
2025-10-16 13:53:52 -0600

    Update urls.py

--- San_alejo/clientes/urls.py
+++ San_alejo/clientes/urls.py
@@ -6,7 +6,7 @@ app_name = 'clientes'
 urlpatterns = [
     path('', views.index, name='clientes'),
     path("listado/", views.listado_clientes, name="listado_clientes"),
-    path('agregar/', views.agregar_cliente, name='agregar_cliente')
-
-
+    path('agregar/', views.agregar_cliente, name='agregar_cliente'),
+    path('detalle/<int:cliente_id>/', views.detalle_cliente, name='detalle_cliente'),
+    path('actualizar/<int:cliente_id>/', views.actualizar_cliente, name='actualizar_cliente'),
 ]
